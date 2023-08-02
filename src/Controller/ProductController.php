ea835bbba9ce4f2513fa336e346a541ca5f98eaf
arvydux <arvydas.kavaliauskas83@gmail.com>
2023-08-02 10:44:31 +0300

    Refactor code

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,7 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
 use App\Services\ProductFetcherService;
-=======
-use App\Services\ProviderService;
->>>>>>> 0ad6fe96a29ef5d684670488eeb144c228862a6d
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,10 +13,6 @@ class ProductController extends AbstractController
     #[Route('/get_product_data', name: 'productData', methods: ['GET'])]
     public function productData(Request $request): Response
     {
-<<<<<<< HEAD
         return (new ProductFetcherService())->getProductDataFromProvider($request);
-=======
-        return (new ProviderService())->getProductDataFromProvider($request);
->>>>>>> 0ad6fe96a29ef5d684670488eeb144c228862a6d
     }
 }
